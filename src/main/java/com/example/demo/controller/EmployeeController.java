6b547f67781bc398605afce753debcf952979c21
funda-radi <radi@funda.kr>
2022-01-06 13:46:04 +0900

    Cors error 간단 해결

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:8080")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> fetchEmployees(){
+        return employeeRepository.findAll();
+    }
+}
